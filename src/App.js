174c536e69d895b8b674862d4a59ecb34f8b2d73
AKAaayush <shresthaaayush81@gmail.com>
2021-08-28 22:25:51 +0545

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Header from "./components/Header";
+import SideNav from "./components/SideNav";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <SideNav />
     </div>
   );
 }
---
+++ src/components/SideNav.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const SideNav = () => {
+  return (
+    <div>
+      <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+        <div class="profile-sidebar">
+          <div class="profile-userpic">
+            <img
+              src="http://placehold.it/50/30a5ff/fff"
+              class="img-responsive"
+              alt=""
+            />
+          </div>
+          <div class="profile-usertitle">
+            <div class="profile-usertitle-name">Username</div>
+            <div class="profile-usertitle-status">
+              <span class="indicator label-success"></span>Online
+            </div>
+          </div>
+          <div class="clear"></div>
+        </div>
+        <div class="divider"></div>
+
+        <ul class="nav menu">
+          <li class="active">
+            <a href="index.html">
+              <em class="fa fa-dashboard">&nbsp;</em> Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <em class="fa fa-cutlery">&nbsp;</em> Product
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <em class="fa fa-calendar">&nbsp;</em> Events
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <em class="fa fa-users">&nbsp;</em> Users
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <em class="fa fa-user">&nbsp;</em> Profile
+            </a>
+          </li>
+
+          <li>
+            <a href="login.html">
+              <em class="fa fa-power-off">&nbsp;</em> Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
